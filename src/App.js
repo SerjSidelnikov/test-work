f863152108c3335a32031ef7a22fc98776cebe43
Serj Sidelnikov <sv@spiks.ru>
2021-01-22 13:50:45 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import {Switch, Route} from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+
+import Catalog from './Catalog';
+import Item from './Item';
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    marginBottom: theme.spacing(2),
+  }
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar position="static" className={classes.header}>
+        <Toolbar>
+          <Typography variant="h3">Catalog</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Container maxWidth="md">
+        <Switch>
+          <Route path={['/', '/catalog']} exact>
+            <Catalog/>
+          </Route>
+          <Route path="/catalog/:id">
+            <Item/>
+          </Route>
+        </Switch>
+      </Container>
+    </>
+  );
+}
+
+export default App;
